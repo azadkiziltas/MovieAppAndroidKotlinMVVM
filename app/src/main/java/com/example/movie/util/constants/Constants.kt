90f18444c83571035492476140e13e4ed20860f4
azadkiziltas <64617883+azadkiziltas@users.noreply.github.com>
2023-09-15 23:04:14 +0300

    Update Constants.kt

--- app/src/main/java/com/example/movie/util/constants/Constants.kt
+++ app/src/main/java/com/example/movie/util/constants/Constants.kt
@@ -2,7 +2,7 @@ package com.example.movie.util.constants
 
 object Constants {
     const val ACCEPT = "application/json"
-    const val AUT = "Bearer YOUR_KEY"
+    const val AUT = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzFhMjgwNmZhOTcwNTVkYWI4ZDE0ODlkZWE1NzFjNSIsInN1YiI6IjY0OGEyZGU2MjkzODM1MDEwMTBkOWQxMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DTMt6t-AbL312So5hi4RuzUg-n071jrWoEQIgE9Hf-I"
     const val PLACE_HOLDER_BASE_URL ="https://api.themoviedb.org/3/"
     const val IMAGE_BASE_URL ="https://image.tmdb.org/"
 
